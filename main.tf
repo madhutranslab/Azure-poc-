1e1b5c8bc4ba4db9e32f3df79f2b57d513bf472c
madhutranslab <150128572+madhutranslab@users.noreply.github.com>
2026-03-08 02:19:16 +0530

    added code

--- main.tf
+++ main.tf
@@ -138,24 +138,54 @@ resource "azurerm_shared_image_version" "linux_image_version" {
     # storage_account_type = "Standard_LRS"
   }
 }
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "myVNet"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  address_space       = ["10.0.0.0/16"]
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "mySubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Network Interface
+resource "azurerm_network_interface" "example" {
+  name                = "myNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+# Linux VM
 resource "azurerm_linux_virtual_machine" "new_vm" {
-  name                  = "newLinuxVM"
+  name                  = "myLinuxVM"
   resource_group_name   = azurerm_resource_group.rg.name
   location              = azurerm_resource_group.rg.location
   size                  = "Standard_B1s"
   admin_username        = "azureuser"
   network_interface_ids = [azurerm_network_interface.example.id]
 
-  source_image_id = azurerm_shared_image_version.linux_image_version.id
-
-  admin_ssh_key {
-    username   = "azureuser"
-    public_key = file("~/.ssh/id_rsa.pub")
-  }
-
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Standard_LRS"
     disk_size_gb         = 30
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "22.04-LTS"
+    version   = "latest"
   }
 }
